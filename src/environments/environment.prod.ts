c89c78539bc44fb76b8271dacde011ec5a62f4cc
Jose Lopez <ernestomx16@gmail.com>
2020-10-01 14:11:38 -0600

    add archivos

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyACisw-pYNEK2mWiyznGkyp4WAhNDviRG8",
+    authDomain: "control-clientes-6cbb2.firebaseapp.com",
+    databaseURL: "https://control-clientes-6cbb2.firebaseio.com",
+    projectId: "control-clientes-6cbb2",
+    storageBucket: "control-clientes-6cbb2.appspot.com",
+    messagingSenderId: "681679222363",
+    appId: "1:681679222363:web:c6ef50ea58d4b1853e317e"
+  }
+};
